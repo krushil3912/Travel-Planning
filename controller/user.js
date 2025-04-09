9748157b9a6bc7bca2b9e469d99bedb69bbb7cf5
krushil3912 <krushildhameliya3912@gmail.com>
2025-04-09 15:41:33 +0530

    Update env travel

--- controller/user.js
+++ controller/user.js
@@ -7,8 +7,8 @@ const transporter = nodemailer.createTransport({
     port: 587,
     secure: false, // true for port 465, false for other ports
     auth: {
-        user: "dhameliyakrushil2023@gmail.com",
-        pass: "pwciyoszdvpkobpu",
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
     },
 });
 
